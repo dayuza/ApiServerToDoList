949a574199f13435e5470ca8044a81635b89c3e6
Teguh Nugratama Dayuza <38966044+dayuza@users.noreply.github.com>
2022-01-25 21:56:53 +0700

    ToDoList

--- api/create.php
+++ api/create.php
@@ -1,12 +1,15 @@
 <?php 
 
+
 include "../config/koneksi.php";
 $nama_barang = @$_POST['waktu'];
 $jumlah_barang = @$_POST['tanggal'];
 $kode_barang = @$_POST['kegiatan'];
 $harga_barang = @$_POST['prioritas'];
 
+
 $data = [];
+
 
 $query = mysqli_query($conn, "INSERT INTO `daily` (`waktu`,`tanggal`,`kegiatan`,`prioritas`) VALUES ('".$nama_barang."', '".$jumlah_barang."', '".$kode_barang."', '".$harga_barang."')" );
 
