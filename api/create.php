ac63a0aaa6a6dbb86680c06937254996fe135437
Teguh Nugratama Dayuza <38966044+dayuza@users.noreply.github.com>
2022-01-26 10:42:10 +0700

    ToDoList

--- api/create.php
+++ api/create.php
@@ -1,18 +1,11 @@
 <?php 
-
-
 include "../config/koneksi.php";
 $nama_barang = @$_POST['waktu'];
 $jumlah_barang = @$_POST['tanggal'];
 $kode_barang = @$_POST['kegiatan'];
 $harga_barang = @$_POST['prioritas'];
-
-
 $data = [];
-
-
 $query = mysqli_query($conn, "INSERT INTO `daily` (`waktu`,`tanggal`,`kegiatan`,`prioritas`) VALUES ('".$nama_barang."', '".$jumlah_barang."', '".$kode_barang."', '".$harga_barang."')" );
-
 if($query){
     $status = true;
     $pesan = "request success";
@@ -21,15 +14,11 @@ if($query){
     $status = false;
     $pesan = "request failed";
 }
-
 $json = [
     "status" => $status,
     "pesan" => $pesan,
     "data" => $data
 ];
-
-
 header("Content-Type: application/json");
 echo json_encode($json);
-
 ?>
